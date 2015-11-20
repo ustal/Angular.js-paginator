000c5b6f3b1457460ebe3fdfd0ce34a61ef7ee92
ustaL <quit.ipad2@gmail.com>
2015-11-20 02:48:08 +0200

    Работает с json файлом, загруженным через $http

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function() {
     var app = angular.module('demo', ['pagination']);
-
+    //var app = angular.module('demo', []);
     app.factory('getData', function () {
             return [
                 {'name': 'Name1'},
@@ -16,21 +16,28 @@
             ]
         });
 
-    app.factory('getDataJson', function($http, $q) {
-            var deferred = $q.defer();
-            $http.jsonp('http://angular.js/paginator/data.json').success(function(data) {
-                deferred.resolve(data);
-            }).error(function(reason) {
-                deferred.reject(reason);
-            });
-            return deferred.promise;
+    app.factory('getDataJson', ['$http', '$q', function($http, $q) {
+        var deferred = $q.defer();
+        $http.get('../paginator/data.json').success(function(data) {
+            deferred.resolve(data);
+        }).error(function(reason) {
+            deferred.reject(reason);
         });
+        return deferred.promise;
+    }]);
 
-    app.controller('demoController', ['$scope', 'getData',  function ($scope, data) {
-            // для пагинатора надо заполнить след данные
-            $scope.perPage = 5;
-            $scope.paginationData = data;
-            $scope.currentPage = 1;
-            //
+    app.controller('demoController', ['$scope', 'getDataJson', function ($scope, getData) {
+        // для пагинатора надо заполнить след данные
+        /*
+            $scope.paginationData = promise или массив данных. Будет доступен в $scope.data
+            $scope.async = true/false/не указывать, если данные получают через promise (например $http.get/post)
+         */
+        $scope.paginationData = getData;
+        $scope.async = true;
+        $scope.perPage = 1;
+        $scope.currentPage = 1;
+            // TODO 3 варианта. Пред-След, пред-цифр-след, цифр
+            // TODO выбор +-1 или +-2 страницы от текущей
+            // end
         }]);
 })();
